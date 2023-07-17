6ae694a219629360ada57d97e3ec50f88838886a
Teymuraz <dehbno4b@gmail.com>
2023-07-17 10:58:39 +0300

    added keys to strokes

--- src/App.js
+++ src/App.js
@@ -1,89 +1,127 @@
-import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
-import { Circle, Polygon, Popup } from 'react-leaflet'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { Circle, Polygon, Popup } from "react-leaflet";
+import { useState, useEffect } from "react";
 
 function App() {
-  let [strikes, setStrikes] = useState([])
-  let [clusters, setClusters] = useState([])
-  const [selectedDay, setSelectedDay] = useState("2022.05.20_12:00_MSC")
-  const [days, setDays] = useState(["2022.05.20_12:00_MSC"])
-
-  const daysCollection = days.map((item) => (<option key={item}>{item}</option>))
-  const url = "http://localhost:9090"
-
-  useEffect(() => {
-    fetch(url + `/daycollection`)
-      .then((response) => response.json())
-      .then((data) => setDays(data))
-  }, []);
+  let [strikes, setStrikes] = useState([]);
+  let [clusters, setClusters] = useState([]);
+  const [date, setDate] = useState();
+  const [time, setTime] = useState("14:00");
+  const url = "http://localhost:9090";
+  const refTime=React.createRef();
+  const refInput=React.createRef();
 
   function loadStrikes(data) {
     if (data != null) {
-      setStrikes(data)
+      setStrikes(data);
     }
   }
   function fetchStrikes() {
-    fetch(url + `/strikes?day=` + selectedDay)
+    fetch(url + `/strikes?day=` + date + " " + time + "_MSC")
       .then((response) => response.json())
-      .then((data) => loadStrikes(data))
-  }
-  function fetchThunders() {
-    fetch(url + `/thunders?day=` + selectedDay)
-      .then((response) => response.json())
-      .then((data) => setClusters(data))
+      .then((data) => loadStrikes(data));
   }
 
-  const clusterStyle = ['red', 'red', 'blue', 'yellow', 'brown', 'black', 'magenta', 'green',
-    'orange', 'Indigo', 'SeaGreen', 'Olive', 'LightSeaGreen', 'Teal', 'Aqua',
-    'Aquamarine', 'CadetBlue', 'SkyBlue', 'DeepSkyBlue', 'MediumSlateBlue',
-    'MidnightBlue', 'BurlyWood', 'Goldenrod', 'Chocolate', 'Sienna', 'Maroon',
-    'MistyRose', 'Gray', 'DarkSlateGray',
-  ]
+  function plus10min() {
+    refInput.current.value=refTime.current.value.toString();
+    const t =Date()
+     t=refTime.current.value
+    t.setTime(14)
+    refTime.current.value=t
+  }
+
+  const clusterStyle = [
+    "red",
+    "red",
+    "blue",
+    "yellow",
+    "brown",
+    "black",
+    "magenta",
+    "green",
+    "orange",
+    "Indigo",
+    "SeaGreen",
+    "Olive",
+    "LightSeaGreen",
+    "Teal",
+    "Aqua",
+    "Aquamarine",
+    "CadetBlue",
+    "SkyBlue",
+    "DeepSkyBlue",
+    "MediumSlateBlue",
+    "MidnightBlue",
+    "BurlyWood",
+    "Goldenrod",
+    "Chocolate",
+    "Sienna",
+    "Maroon",
+    "MistyRose",
+    "Gray",
+    "DarkSlateGray",
+  ];
   const lightning = strikes.map((item) => (
-    <Circle key={item.Longitude + item.Latitude}
+    <Circle
+      key={item.Id}
       pathOptions={{
         color: clusterStyle[Number(item.Cluster)],
 
         fillColor: clusterStyle[Number(item.Cluster)],
       }}
-      eventHandlers={{ click: () => { console.log(item) } }}
+      eventHandlers={{
+        click: () => {
+          console.log(item);
+        },
+      }}
       center={[item.Latitude, item.Longitude]}
-      radius={1000}></Circle>
-  ))
-  const thunders = clusters.map((item) => (<Polygon key={item.Claster}
-    pathOptions={{ color: "DarkSlateGray" }}
-    //eventHandlers ={{click:()=>{console.log(item.Claster)}}}
-    positions={item.Polygon}>
-    <Popup>
-      Cluster:{item.Claster} <br />
-      Area:{item.area}<br />
-      Capacity: {item.capacity}<br />
-      Start time: {item.starttime}<br />
-      End time: {item.endtime}
-    </Popup>
-  </Polygon>))
+      radius={1000}
+    ></Circle>
+  ));
+  const thunders = clusters.map((item) => (
+    <Polygon
+      key={item.Claster}
+      pathOptions={{ color: "DarkSlateGray" }}
+      //eventHandlers ={{click:()=>{console.log(item.Claster)}}}
+      positions={item.Polygon}
+    >
+      <Popup>
+        Cluster:{item.Claster} <br />
+        Area:{item.area}
+        <br />
+        Capacity: {item.capacity}
+        <br />
+        Start time: {item.starttime}
+        <br />
+        End time: {item.endtime}
+      </Popup>
+    </Polygon>
+  ));
 
   return (
     <div className="App">
       <button onClick={fetchStrikes}>fetch strikes</button>
-      <button onClick={fetchThunders}>fetch thunders</button>
-      <select
+      {/* <button onClick={fetchThunders}>fetch thunders</button> */}
+      {/* <select
         onChange={e => {
           setSelectedDay(e.target.value)
         }}
       >
 
         {daysCollection}
-      </select>
+      </select> */}
+      <input type="text" name="label" defaultValue="sdfgs" ref={refInput}></input>
       <label>
         data:
-        <input onChange={e => {
-          setSelectedDay(e.target.value)
-        }}
-          type="text" name="name" />
+        <input type="datetime" name="date" defaultValue="2022-05-20"  />
       </label>
-      <MapContainer center={[50.505, 40.00]} zoom={5} scrollWheelZoom={true}>
+      <label>
+        time:
+        <input type="time" name="time" ref={refTime} defaultValue="12:00" step="10" />
+      </label>
+      <button onClick={plus10min}>+10min</button>
+      <MapContainer center={[50.505, 40.0]} zoom={5} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
